b31f1fa9f6a50531237228472b02b472d335ef97
Parastoo <bright.sunrise33@gmail.com>
2024-08-16 13:12:38 +0330

    hello project tidy up

---
+++ hello_rust/target/debug/deps/hello_cargo-96c22bc1e6d9a165.d
@@ -0,0 +1,5 @@
+/media/parastoo/D/Rust_Basics/hello_rust/target/debug/deps/hello_cargo-96c22bc1e6d9a165: src/main.rs
+
+/media/parastoo/D/Rust_Basics/hello_rust/target/debug/deps/hello_cargo-96c22bc1e6d9a165.d: src/main.rs
+
+src/main.rs:
---
+++ hello_rust/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/media/parastoo/D/Rust_Basics/hello_rust/target/debug/hello_cargo: /media/parastoo/D/Rust_Basics/hello_rust/src/main.rs
