c80e72118dcb1945fcfa22316e0572c119d9b8e3
Parastoo <bright.sunrise33@gmail.com>
2024-10-20 17:50:39 +0330

    pass struct to the method

--- struct_basics/src/main.rs
+++ struct_basics/src/main.rs
@@ -7,24 +7,34 @@ struct User {
 }
 
 fn main() {
-    // let user1 = User {
-    //     active: true,
-    //     username: String::from("someusername123"),
-    //     email: String::from("someone@example.com"),
-    //     sign_in_count: 1,
-    // };
 
     let mut user1 = User {
         active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
+        username: String::from("user1"),
+        email: String::from("anotheremail@example.com"),
         sign_in_count: 1,
     };
 
-    user1.email = String::from("anotheremail@example.com");
+    user1.email = String::from("user1email@example.com");
     
     println!("{0}", user1.active);
     println!("{0}", user1.username);
     println!("{0}", user1.email);
-    println!("{0}", user1.sign_in_count);
+    println!("{0}\n", user1.sign_in_count);
+
+    let user2 = build_user("user2email@example.com".to_string(), "user2".to_string());
+    
+    println!("{0}", user2.active);
+    println!("{0}", user2.username);
+    println!("{0}", user2.email);
+    println!("{0}", user2.sign_in_count);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 2,
+    }
 }
