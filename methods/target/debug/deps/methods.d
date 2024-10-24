8701dae4774ae719c3da8f06015b90af47bc1583
Parastoo <bright.sunrise33@gmail.com>
2024-10-24 09:48:19 +0330

    method introduction

---
+++ methods/target/debug/deps/methods.d
@@ -0,0 +1,5 @@
+D:\Rust\Rust_Basics\methods\target\debug\deps\methods.exe: src/main.rs
+
+D:\Rust\Rust_Basics\methods\target\debug\deps\methods.d: src/main.rs
+
+src/main.rs:
---
+++ methods/target/debug/methods.d
@@ -0,0 +1,1 @@
+D:\Rust\Rust_Basics\methods\target\debug\methods.exe: D:\Rust\Rust_Basics\methods\src\main.rs
