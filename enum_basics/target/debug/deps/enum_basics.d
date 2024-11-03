25c061f6462f9abb63a7c2615ed3efda7a9c0a30
Parastoo <bright.sunrise33@gmail.com>
2024-11-03 16:29:48 +0330

    enum basic concept

---
+++ enum_basics/target/debug/deps/enum_basics.d
@@ -0,0 +1,5 @@
+D:\Rust\Rust_Basics\enum_basics\target\debug\deps\enum_basics.exe: src/main.rs
+
+D:\Rust\Rust_Basics\enum_basics\target\debug\deps\enum_basics.d: src/main.rs
+
+src/main.rs:
---
+++ enum_basics/target/debug/enum_basics.d
@@ -0,0 +1,1 @@
+D:\Rust\Rust_Basics\enum_basics\target\debug\enum_basics.exe: D:\Rust\Rust_Basics\enum_basics\src\main.rs
