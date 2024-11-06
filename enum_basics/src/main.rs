305c51797a5639bca15374742532cf456801cca8
Parastoo <bright.sunrise33@gmail.com>
2024-11-06 22:38:33 +0330

    enum practice

--- enum_basics/src/main.rs
+++ enum_basics/src/main.rs
@@ -5,9 +5,25 @@ enum Coin{
     Dime,
     Quarter,
 }
+enum IpAddrKind1 {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+//let home = IpAddrKind1::V4(192, 168, 3, 10);
+//let loopback = IpAddrKind1::V6(String::from("::1"))
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 
 fn main() {
     let ret;
+
     ret = value_in_cents(Coin::Nickel);
     println!("{ret}");
 }
